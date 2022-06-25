fce6f8a4e9bbd9106475a554d3dd2f47ef8ea7b1
Ojongseok <whdtjr6889@naver.com>
2022-06-25 15:30:24 +0900

    bottomNav setting

---
+++ app/src/main/java/com/example/dabang_aromprj/Home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.dabang_aromprj.Home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dabang_aromprj.R
+
+class HomeFragment : Fragment() {
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/com/example/dabang_aromprj/MainActivity.kt
+++ app/src/main/java/com/example/dabang_aromprj/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.dabang_aromprj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dabang_aromprj.Board.BoardFragment
+import com.example.dabang_aromprj.Book.BookFragment
+import com.example.dabang_aromprj.Category.CategoryFragment
+import com.example.dabang_aromprj.Home.HomeFragment
+import com.example.dabang_aromprj.Myinfo.MyinfoFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_container,HomeFragment()).commit()
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container,HomeFragment()).commit()
+                }
+                R.id.item2 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container,CategoryFragment()).commit()
+                }
+                R.id.item3 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container,BoardFragment()).commit()
+                }
+                R.id.item4 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container,BookFragment()).commit()
+                }
+                R.id.item5 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container,MyinfoFragment()).commit()
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
